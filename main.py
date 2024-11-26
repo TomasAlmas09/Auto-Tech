c072fc2f60d93fa932a1879179e2449dd490ef11
oscar523 <oscarnascimento52322@gmail.com>
2024-11-26 16:40:54 +0000

    AddFiles

--- main.py
+++ main.py
@@ -1,18 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
+
+class RegistryForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])  
+    email = StringField('Email', validators=[DataRequired(), Email()])  
+    senha = PasswordField('Senha', validators=[DataRequired()])  
+    confirmar_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha', message='As senhas devem coincidir.')]) 
+    submit = SubmitField('Registrar')  
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')  
+    name = None
+    form = LoginForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('login.html', form=form, name=name)
 
-@app.route('/registry')
+@app.route('/registry', methods=['GET', 'POST'])
 def registry():
-    return render_template('registry.html') 
+    form = RegistryForm()
+    
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        senha = form.senha.data
+        
+        flash(f'Conta criada com sucesso para {nome}!', 'success')
+        
+       
+        return render_template('registry.html')
+    
+    return render_template('registry.html', form=form) 
 
 @app.route('/product-single')
 def product_single():
